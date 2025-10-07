96d1365249687edd459573d84deeeb759d30122d
Shaurya Aggarwal <shaurya_2301ee56@iitp.ac.in>
2025-10-07 22:45:53 +0530

    bug fixes

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -1,10 +1,10 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
+import toast from 'react-hot-toast';
+
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 axios.defaults.baseURL = API_URL;
-
-import toast from 'react-hot-toast';
 
 const AuthContext = createContext();
 
