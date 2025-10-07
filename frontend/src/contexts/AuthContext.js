0f5d5de66639fb17341bc3721d882416275d6da0
Shaurya Aggarwal <shaurya_2301ee56@iitp.ac.in>
2025-10-07 23:12:51 +0530

    updated api base url in frontend

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import toast from 'react-hot-toast';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL;
 axios.defaults.baseURL = API_URL;
 
 const AuthContext = createContext();
